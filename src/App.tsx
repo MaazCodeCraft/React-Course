cb485bdf86c83c2053265aea9e1aba781a730c54
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-07-01 19:09:46 +0500

    Passing Children

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "London", "Paris", "Tokyo", "Islamabad"];
-
-  let HandleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={HandleSelectItem}
-      />
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
