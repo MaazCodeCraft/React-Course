d78d23294d997d927414263f82fc45e9ef86e09d
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-07-02 16:09:39 +0500

    Exercise- Showing an Alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisible] = useState(false);
+
   return (
     <div>
-      <Button color="primary" onClick={() => console.log("Clicked")}>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisible(true)}>
         My Button
       </Button>
     </div>
